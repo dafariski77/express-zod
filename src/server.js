26e4497260605f95ca400b406f25287192c77feb
dafariski77 <rskdafa@gmail.com>
2024-01-08 18:14:50 +0700

    feat(socket): add jwt verify

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import userRoute from "./api/v1/user/user.route.js";
 import errorHandlerMiddleware from "./middlewares/handleError.js";
 import prisma from "./lib/prisma.js";
 import roomRoute from "./api/v1/room/room.route.js";
+import { isTokenValid } from "./lib/jwt.js";
 
 dotenv.config();
 
@@ -28,8 +29,29 @@ app.use(errorHandlerMiddleware);
 const server = http.createServer(app);
 const io = new Server(server);
 
+io.use((socket, next) => {
+  try {
+    const token =
+      socket.handshake.auth?.token || socket.handshake.headers?.token;
+
+    if (!token) {
+      throw new Error("Authentication error: Token not provided");
+    }
+
+    const payload = isTokenValid({ token });
+
+    const userId = payload.id;
+
+    socket.userId = userId;
+
+    next();
+  } catch (error) {
+    next(new Error("Authentication error: Invalid token"));
+  }
+});
+
 io.on("connection", (socket) => {
-  console.log("A user connected");
+  console.log(`A user ${socket.userId} connected`);
 
   socket.on("joinRoom", async ({ roomId, userId }) => {
     socket.join(roomId);
